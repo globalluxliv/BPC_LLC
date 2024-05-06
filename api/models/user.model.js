d8da14886068fe516045ec5abc449cce08e9c265
freddycmh <50223798+freddycmh@users.noreply.github.com>
2024-05-06 14:44:57 -0400

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        unique: true,
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
