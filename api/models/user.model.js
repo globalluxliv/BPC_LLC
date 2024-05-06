244056de1acbf3caedef0bafdddefd193573c075
freddycmh <50223798+freddycmh@users.noreply.github.com>
2024-05-06 15:40:28 -0400

    tried adding user data

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    userName: {
+    username: {
         type: String,
         required: true,
         unique: true,
