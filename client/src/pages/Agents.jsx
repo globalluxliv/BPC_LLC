ce8cec703c42a2373470bddffd1d829dbfd7c3c3
freddycmh <50223798+freddycmh@users.noreply.github.com>
2024-05-17 14:03:05 -0400

    complete create listing UI

--- client/src/pages/Agents.jsx
+++ client/src/pages/Agents.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import MikeImage from '../photos/Mike.png';
+import { FaPhone } from "react-icons/fa6";
+
 
 export default function Agents() {
   return (
@@ -15,7 +17,8 @@ export default function Agents() {
     {/* first person */}
     <div class="flex flex-col md:flex-row gap-6">
       <div class="w-full lg:w-1/4 rounded-3xl overflow-hidden">
-              <img src={MikeImage} alt="https://images.pexels.com/photos/1036622/pexels-photo-1036622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="w-full h-full object-cover aspect-video lg:aspect-square"/>
+              <img src={MikeImage} alt="https://images.pexels.com/photos/1036622/pexels-photo-1036622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                class="w-full h-full object-contain aspect-video lg:aspect-square" />
       </div>
       <div class="w-full lg:w-9/12 bg-blue-100 rounded-3xl flex flex-col justify-center p-8 lg:p-14">
         <h3 class="text-2xl capitalize text-indigo-900 font-semibold">Akm Mike Bhuiyan</h3>
@@ -24,8 +27,11 @@ export default function Agents() {
                As a resident of New York for over 34 years, Mike Bhuiyan has extensive experience specializing in the ins and outs of this city. As a top-producing broker, his goal is to find his clients the best solutions using his dynamic ability to anticipate his client’s needs and exceeding their expectations every single time. Whether dealing with buyers or sellers, his strong knowledge of New York City’s neighborhoods, combined with his consistent history of making a deal happen, has earned him his growing success and set him apart as a top-ranking broker at DJK Residential. Prior to his successful real estate career, Mike has owned a national designer fashion business in the Garment District for over a decade. He is also very dedicated to community service. Utilizing his fluency in English, Bengali, Hindi, Urdu, Spanish, and Portuguese, Mike has travelled to over 82 countries with various interests and has established a school and college for the economically disadvantaged. In his free time, he is a Certified Leadership Trainer and Senator of the Junior Chamber International, Past District Governor for Lions Clubs International District 20-R2 New York, Progressive Melvin Jones Fellow of Lions Clubs International, Founder Presidents [ of District 20-R2 Foundation, Manhattan Downtown Community Alliance, Manhattan Downtown Multicultural Food Pantry and member of Real Estate Board of New York, Manhattan Association of Realtors, New York State Association of Realtors and National Association of Realtors.
 
               </p>
+              <br/>
+              <p>Phone: 212.884.2211</p>
+              <email> Email: mbhuiyan@bpcresidential.com</email>
             </div>
-            
+          
     </div>
     
           {/* second person */}
