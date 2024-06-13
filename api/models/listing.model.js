acc817eb70ceb01b9228f3cf1199068cc24b811d
jairoMolina9 <molinaandres9991@gmail.com>
2024-06-12 20:14:10 -0400

    added map component to each listing using embedded html

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -54,6 +54,10 @@ const listingSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    mapUrl: {
+      type: String,
+      required: false,
+    },
   },
   { timestamps: true }
 );
