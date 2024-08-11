c780c8d240cf01d904e48a977688985100f3b271
freddycmh <50223798+freddycmh@users.noreply.github.com>
2024-08-11 15:47:12 -0400

    take out search bar no need

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,14 +24,14 @@ export default function Header() {
     }
   }, [location.search]);
   return (
-    <header className="bg-slate-200 shadow-md ">
+    <header className="bg-slate-200 shadow-md  ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-slate-500">Global Luxury Livings </span>
           </h1>
         </Link>
-        <form
+        {/* <form
           onSubmit={handleSubmit}
           className="bg-slate-100 p-3 rounded-lg flex items-center"
         >
@@ -45,7 +45,7 @@ export default function Header() {
           <button>
             <FaSearch className="text-slate-600" />
           </button>
-        </form>
+        </form> */}
         <ul className="flex gap-4">
           <Link to="/">
             <li className=" text-slate-700 hover:underline">Home</li>
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -122,7 +122,7 @@ export default function Search() {
     <div className="flex flex-col md:flex-row">
       <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
         <form onSubmit={handleSubmit} className="flex flex-col gap-8">
-          <div className="flex items-center gap-2">
+          {/* <div className="flex items-center gap-2">
             <label className="whitespace-nowrap font-semibold">
               Search Term:
             </label>
@@ -134,7 +134,7 @@ export default function Search() {
               value={sidebardata.searchTerm}
               onChange={handleChange}
             />
-          </div>
+          </div> */}
           <div className="flex gap-2 flex-wrap items-center">
             <label className="font-semibold">Type:</label>
             <div className="flex gap-2">
