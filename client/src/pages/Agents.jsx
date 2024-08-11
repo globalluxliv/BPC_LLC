835ab22a9eff24593dbc2a3796d635df6373650c
freddycmh <50223798+freddycmh@users.noreply.github.com>
2024-08-11 10:05:11 -0400

    updated agent page with a button

--- client/src/pages/Agents.jsx
+++ client/src/pages/Agents.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import MikeImage from "../photos/Mike.png";
 import QueenieImage from "../photos/Queenie.png";
-
 import Footer from "../components/Footer";
 
 export default function Agents() {
@@ -28,13 +27,9 @@ export default function Agents() {
               />
             </div>
             <div className="w-full lg:w-9/12 bg-blue-100 rounded-3xl flex flex-col justify-center p-8 lg:p-14">
-              <Link to="/agent/664a1c34413c39f3d7fa02d4">
-                {" "}
-                {/* Use the actual user ID here */}
-                <h3 className="text-2xl capitalize text-indigo-900 font-semibold">
-                  Akm Mike Bhuiyan
-                </h3>
-              </Link>
+              <h3 className="text-2xl capitalize text-indigo-900 font-semibold">
+                Akm Mike Bhuiyan
+              </h3>
               <span className="inline-block capitalize text-xl text-indigo-900 font-light mt-1.5 mb-5">
                 Licensed Principal Broker
               </span>
@@ -50,51 +45,48 @@ export default function Agents() {
               <br />
               <p>Phone: 212.884.2211</p>
               <p>Email: mbhuiyan@bpcresidential.com</p>
+              <Link to="/agent/664a1c34413c39f3d7fa02d4">
+                <button className="mt-4 px-4 py-2 bg-blue-800 text-white font-bold rounded hover:bg-blue-700">
+                  View Listings
+                </button>
+              </Link>
             </div>
           </div>
         </div>
 
-        {/* New Agent: Jane Doe */}
+        {/* Queenie Ma */}
         <div className="flex flex-col gap-6 mt-16">
           <div className="flex flex-col md:flex-row gap-6">
             <div className="w-full lg:w-1/4 rounded-3xl overflow-hidden">
               <img
                 src={QueenieImage}
-                alt="Jane Doe"
+                alt="Queenie Ma"
                 className="w-full h-full object-contain aspect-video lg:aspect-square"
               />
             </div>
             <div className="w-full lg:w-9/12 bg-blue-100 rounded-3xl flex flex-col justify-center p-8 lg:p-14">
-              <Link to="/agent/66a6a7c652f384512b70c15f">
-                {" "}
-                {/* Use the actual user ID here */}
-                <h3 className="text-2xl capitalize text-indigo-900 font-semibold">
-                  Lixi Ma (Queenie)
-                </h3>
-              </Link>
+              <h3 className="text-2xl capitalize text-indigo-900 font-semibold">
+                Lixi Ma (Queenie)
+              </h3>
               <span className="inline-block capitalize text-xl text-indigo-900 font-light mt-1.5 mb-5">
                 Licensed Sales Person
               </span>
               <p className="text-indigo-900 opacity-75 leading-normal">
-                {/* Jane's description */}
+                {/* Queenie's description */}
                 Queenie Ma is a Real Estate professional licensed sales agent in
                 New York City for over 12 years. Her expertise are sale,
                 purchase, rental in residential, commercial and management
                 condominiums and buildings. Queenie lives in the Battery Park
-                City more than 25 years. She has grown to love, living in New
-                York City after immigrated from China. For all these years live
-                in the City, she has developed strong relationships with
-                clients, owners, and real estate community fellows. Queenie is a
-                very active and more productive RE professional in New York. She
-                treats all her local and international clients as family members
-                and willing to provide additional supports in the field. I am
-                proud to be a team member of Battery Park City Residential LLC.
-                You’re very welcome to contact us for your Real Estate needs and
-                get some professional advices. Hope to meet you soon!
+                City more than 25 years...
               </p>
               <br />
               <p>Phone: 646.285.8277</p>
               <p>Email: queenie@bpcresidential.com</p>
+              <Link to="/agent/66a6a7c652f384512b70c15f">
+                <button className="mt-4 px-4 py-2 bg-blue-800 text-white font-bold rounded hover:bg-blue-700">
+                  View Listings
+                </button>
+              </Link>
             </div>
           </div>
         </div>
