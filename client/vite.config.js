eec93131a1629de44a663f249a22e790a6696dcd
AdamAlbaghali <adamalbaghali@gmail.com>
2025-07-19 14:30:22 -0400

    Fix: Set Vite output dir to dist for Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         secure: false,
       },
     },
+  },
+  build: {
+    outDir: "dist",
   },
   plugins: [react()],
 });
