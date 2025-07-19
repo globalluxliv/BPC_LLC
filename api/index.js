efab3268590cbd31e4a1fc8a002acaa03c5bdc72
AdamAlbaghali <adamalbaghali@gmail.com>
2025-07-19 14:15:42 -0400

    Added a root route for testing

--- api/index.js
+++ api/index.js
@@ -40,6 +40,10 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
