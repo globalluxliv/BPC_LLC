18f7c89d12122d5fe56a6d9f2aaea7e788100d6f
AdamAlbaghali <adamalbaghali@gmail.com>
2025-07-19 14:37:23 -0400

    Fix Express static path to client/dist

--- api/index.js
+++ api/index.js
@@ -34,10 +34,10 @@ app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 app.use("/api/review", reviewRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 app.get("/", (req, res) => {
